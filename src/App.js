6566d25083076d34483cf0254cb3a53dd826c42e
seerosecode <keshalakemusic@gmail.com>
2019-10-07 23:42:09 -0400

    character table rendering. app complete. no bugs. Could stand a bit more styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Button } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PeopleTable from './components/PeopleTable';
+import CharacterTable from './components/CharacterTable'
 const apiKey = process.env.REACT_APP_API_KEY;
 const proxyURL = "https://cors-anywhere.herokuapp.com/";
 const apiURL = "https://api.salesloft.com/v2/people";
@@ -16,9 +17,9 @@ class App extends Component{
   }
 
   state = {
-    people: []
+    people: [],
+    display: false
   }
-
 
   fetchPeople = () => {
     fetch(proxyURL + apiURL, {
@@ -28,24 +29,49 @@ class App extends Component{
       }
     })
     .then(res => res.json())
-    .then(res => {this.setState({people: res.data})}
+    .then(res => {this.setState({...this.state, people: res.data})}
     )
   }
 
+  collectEmails = () => {
+    let allEmails = []
+      this.state.people.forEach((person) => {
+        allEmails.push(person.email_address)
+      })
+    return allEmails
+  }
+
   characterCount = () => {
-    console.log("VOILA! EMAIL CHARACTERS!")
+    let characters = {}
+    let emails = this.collectEmails().join()
+    emails.split("").map((char) => {
+      characters[char] ? characters[char] += 1 : characters[char] = 1
+    })
+    return characters
+  }
+
+  toggleTable = () => {
+    this.setState({...this.state, display: !this.state.display})
+  }
+
+  toggleButton = () => {
+    if (this.state.display)
+      return "Close"
+    else
+      return "Character Count"
   }
 
   render(){
-  return (
-    <div className="App">
-    <header>
-      <Button onClick={this.characterCount}>Character Count</Button>
-    </header>
+    return (
+      <div className="App">
+      <header>
+        <Button onClick={this.toggleTable}>{this.toggleButton()}</Button>
+        {this.state.display ? <CharacterTable count={this.characterCount()} className="character-table" /> : null}
+      </header>
         <PeopleTable people={this.state.people}/>
-    </div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 export default App;
